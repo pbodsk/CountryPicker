bdd076443fb9dffb0105b70cbf61525da77a402a
Peter Bødskov <pb@reeltrak.com>
2019-10-02 22:04:46 +0200

    fixing path again

--- Package.swift
+++ Package.swift
@@ -20,9 +20,11 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "CountryPicker",
-            dependencies: []),
+            dependencies: [],
+            path: "CountryPicker/Sources"),
         .testTarget(
             name: "CountryPickerTests",
-            dependencies: ["CountryPicker"]),
+            dependencies: ["CountryPicker"],
+            path: "CountryPicker/Tests"),
     ]
 )
